7cdc18e341fcf721a7d4ab9a1e84e1c6f60dc720
yeojin5 <yeojinoh@dankook.ac.kr>
2024-10-13 21:34:11 +0900

    add mean hops benchmark

--- include/percentile_stats.h
+++ include/percentile_stats.h
@@ -18,81 +18,87 @@
 #include "parameters.h"
 
 namespace diskann {
-  struct QueryStats {
-    float total_us = 0;  // total time to process query in micros
-    float io_us = 0;     // total time spent in IO
-    float cpu_us = 0;    // total time spent in CPU
+	struct QueryStats {
+		float total_us = 0;  // total time to process query in micros
+		float io_us = 0;     // total time spent in IO
+		float cpu_us = 0;    // total time spent in CPU
 
-    unsigned n_4k = 0;          // # of 4kB reads
-    unsigned n_8k = 0;          // # of 8kB reads
-    unsigned n_12k = 0;         // # of 12kB reads
-    unsigned n_ios = 0;         // total # of IOs issued
-    unsigned read_size = 0;     // total # of bytes read
-    unsigned n_cmps_saved = 0;  // # cmps saved
-    unsigned n_cmps = 0;        // # cmps
-    unsigned n_cache_hits = 0;  // # cache_hits
-    unsigned n_hops = 0;        // # search hops
-  };
+		unsigned n_4k = 0;          // # of 4kB reads
+		unsigned n_8k = 0;          // # of 8kB reads
+		unsigned n_12k = 0;         // # of 12kB reads
+		unsigned n_ios = 0;         // total # of IOs issued
+		unsigned read_size = 0;     // total # of bytes read
+		unsigned n_cmps_saved = 0;  // # cmps saved
+		unsigned n_cmps = 0;        // # cmps
+		unsigned n_cache_hits = 0;  // # cache_hits
+		unsigned n_hops = 0;        // # search hops
+	};
 
-  template<typename T>
-  inline T get_percentile_stats(
-      QueryStats *stats, uint64_t len, float percentile,
-      const std::function<T(const QueryStats &)> &member_fn) {
-    std::vector<T> vals(len);
-    for (uint64_t i = 0; i < len; i++) {
-      vals[i] = member_fn(stats[i]);
-    }
+	template<typename T>
+		inline T get_percentile_stats(
+				QueryStats *stats, uint64_t len, float percentile,
+				const std::function<T(const QueryStats &)> &member_fn) {
+			std::vector<T> vals(len);
+			for (uint64_t i = 0; i < len; i++) {
+				vals[i] = member_fn(stats[i]);
+			}
 
-    std::sort(vals.begin(), vals.end(),
-              [](const T &left, const T &right) { return left < right; });
+			std::sort(vals.begin(), vals.end(),
+					[](const T &left, const T &right) { return left < right; });
 
-    auto retval = vals[(uint64_t)(percentile * len)];
-    vals.clear();
-    return retval;
-  }
+			auto retval = vals[(uint64_t)(percentile * len)];
+			vals.clear();
+			return retval;
+		}
 
-  template<typename T>
-  inline double get_mean_stats(
-      QueryStats *stats, uint64_t len,
-      const std::function<T(const QueryStats &)> &member_fn) {
-    double avg = 0;
-    for (uint64_t i = 0; i < len; i++) {
-      avg += (double) member_fn(stats[i]);
-    }
-    return avg / len;
-  }
+	template<typename T>
+		inline double get_mean_stats(
+				QueryStats *stats, uint64_t len,
+				const std::function<T(const QueryStats &)> &member_fn) {
+			double avg = 0;
+			for (uint64_t i = 0; i < len; i++) {
+				avg += (double) member_fn(stats[i]);
+			}
+			return avg / len;
+		}
 
-  // The following two functions are used when getting statistics while range searching on only queries with
-  // non-zero gt lengths
-  template<typename T>
-  inline T get_percentile_stats_gt(
-      QueryStats *stats, uint64_t len, float percentile,
-      const std::function<T(const QueryStats &)> &member_fn, std::vector<std::vector<uint32_t>> &gt) {
-    std::vector<T> vals;
-    for (uint64_t i = 0; i < len; i++) {
-      if (gt[i].size()) vals.push_back(member_fn(stats[i]));
-    }
+	// The following two functions are used when getting statistics while range searching on only queries with
+	// non-zero gt lengths
+	template<typename T>
+		inline T get_percentile_stats_gt(
+				QueryStats *stats, uint64_t len, float percentile,
+				const std::function<T(const QueryStats &)> &member_fn, std::vector<std::vector<uint32_t>> &gt) {
+			std::vector<T> vals;
+			for (uint64_t i = 0; i < len; i++) {
+				if (gt[i].size()) vals.push_back(member_fn(stats[i]));
+			}
 
-    std::sort(vals.begin(), vals.end(),
-              [](const T &left, const T &right) { return left < right; });
+			std::sort(vals.begin(), vals.end(),
+					[](const T &left, const T &right) { return left < right; });
 
-    auto retval = vals[(uint64_t)(percentile * vals.size())];
-    vals.clear();
-    return retval;
-  }
+			auto retval = vals[(uint64_t)(percentile * vals.size())];
+			vals.clear();
+			return retval;
+		}
 
-  template<typename T>
-  inline double get_mean_stats_gt(
-      QueryStats *stats, uint64_t len,
-      const std::function<T(const QueryStats &)> &member_fn, std::vector<std::vector<uint32_t>> &gt) {
-    uint32_t cnt = 0;
-    double avg = 0;
-    for (uint64_t i = 0; i < len; i++) {
-      if (gt[i].size()) {
-        ++cnt;
-        avg += (double) member_fn(stats[i]);
-      }
-    }
-    return avg / cnt;
-  }
+	template<typename T>
+		inline double get_mean_stats_gt(
+				QueryStats *stats, uint64_t len,
+				const std::function<T(const QueryStats &)> &member_fn, std::vector<std::vector<uint32_t>> &gt) {
+			uint32_t cnt = 0;
+			double avg = 0;
+			for (uint64_t i = 0; i < len; i++) {
+				if (gt[i].size()) {
+					++cnt;
+					avg += (double) member_fn(stats[i]);
+				}
+			}
+			return avg / cnt;
+		}
+	template<typename T>
+		inline unsigned int get_mean_stats_hop(
+				QueryStats *stats, uint64_t len, 
+				const std::function<T(const QueryStats &)> &member_fn, std::vector<std::vector<uint32_t>> &gt){
+		double avg = 0;
+} 
 }  // namespace diskann
